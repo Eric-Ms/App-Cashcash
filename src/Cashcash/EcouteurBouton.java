114a62a18b57dd977379aa67cdf3be0f0c3a1310
Eric-Ms <ericmaes.pro@gmail.com>
2022-04-25 19:15:23 +0200

    Génération du fichier XML

--- src/Cashcash/EcouteurBouton.java
+++ src/Cashcash/EcouteurBouton.java
@@ -21,7 +21,48 @@ public class EcouteurBouton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e){
         if (e.getActionCommand().equals("Générer un fichier XML")){
-            System.out.println("Le fichier à été généré");
+
+
+            try {
+
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            // Ajout de l'élément principal
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("Cashcash");
+            doc.appendChild(rootElement);
+
+            // staff elements
+            Element staff = doc.createElement("Membres");
+            rootElement.appendChild(staff);
+
+            // Ajout de l'élément STAFF
+            Attr attr = doc.createAttribute("id");
+            attr.setValue("1");
+            staff.setAttributeNode(attr);
+
+            // Ajout de l'élément Prénom
+            Element firstname = doc.createElement("Prénom");
+            firstname.appendChild(doc.createTextNode("Eric"));
+            staff.appendChild(firstname);
+
+            // Ajouter du contenu dans le document XML
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("monFichier.xml"));
+
+            transformer.transform(source, result);
+
+            System.out.println("Le fichier à été généré avec succès !");
+
+            // Codes d'erreurs
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
         }
     }
 }
