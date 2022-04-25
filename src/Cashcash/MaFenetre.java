d5493c92bb13cf6e51ed7d395ae5455d65546a79
Eric-Ms <ericmaes.pro@gmail.com>
2022-04-25 19:27:58 +0200

    Ajout nom de famille XML

--- src/Cashcash/MaFenetre.java
+++ src/Cashcash/MaFenetre.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class MaFenetre extends JFrame{
     private JPanel pan = new JPanel();
     JButton boutonXML = new JButton("Générer un fichier XML");
+    JButton boutonTwo = new JButton("Deuxième bouton");
 
 
     public MaFenetre(){
@@ -11,6 +12,8 @@ public class MaFenetre extends JFrame{
         setTitle("CashCashApp");
         pan.add(boutonXML);
         boutonXML.addActionListener(new EcouteurBouton());
+        pan.add(boutonTwo);
+        boutonTwo.addActionListener(new EcouteurBouton());
         this.setContentPane(pan);
         this.setVisible(true);
 
