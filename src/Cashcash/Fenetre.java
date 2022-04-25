5f333ed0052b7ed51530f1fda5ac52dcc80b684b
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2022-04-25 18:53:47 +0200

    Main

---
+++ src/Cashcash/Fenetre.java
@@ -0,0 +1,13 @@
+package Cashcash;
+import javax.swing.*;
+
+public class MaFenetre extends JFrame{
+    private JPanel pan = new JPanel();
+
+
+    public MaFenetre(){
+        setSize(400, 200);
+        setTitle("CashCashApp");
+
+    }
+}
