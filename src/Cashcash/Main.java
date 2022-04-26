26feb95789a321aba3aa99b666d7d4394063f29d
Eric-Ms <ericmaes.pro@gmail.com>
2022-04-26 19:43:37 +0200

    Essai de la connexion à la BDD

--- src/Cashcash/Main.java
+++ src/Cashcash/Main.java
@@ -1,10 +1,31 @@
 package Cashcash;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Connection cnx = connecterDB();
+
         MaFenetre fen = new MaFenetre();
         fen.setVisible(true);
+    }
+
+    public static Connection connecterDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver OK !");
+            String url="jdbc:mysql://localhost:3306/cashcash";
+            String user="root";
+            String password="";
+            Connection cnx=DriverManager.getConnection(url, user, password);
+            System.out.println("Connexion à la base de donnée établie.");
+            return cnx;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
