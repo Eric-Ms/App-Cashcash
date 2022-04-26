f646f0b4729262731325367a6c620da16f2b201d
Eric-Ms <ericmaes.pro@gmail.com>
2022-04-26 19:23:33 +0200

    Suppr inutile

--- src/Cashcash/Main.java
+++ src/Cashcash/Main.java
@@ -3,7 +3,6 @@ package Cashcash;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         MaFenetre fen = new MaFenetre();
         fen.setVisible(true);
